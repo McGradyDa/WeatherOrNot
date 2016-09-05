4a1f3b58eecbc91fbb5b76c3fb079403cdde50f5
tracyda <tracyliubai@gmail.com>
2016-09-05 23:42:27 +0800

    Fix get city data 1

--- Weather/Weather/CityDataSource.cs
+++ Weather/Weather/CityDataSource.cs
@@ -6,18 +6,21 @@ namespace Weather
 {
     public static class CityDataSource
     {
-        private static List<CityData.City> CitySource = CityData.CitysOfCountry.city.OrderBy(c => c.name).ToList();
-        public static List<CityData.City> Citys
+        private static List<CityData.Place> CitySource = CityData.CitysOfCountry.query.results.place.OrderBy(c => c.name).ToList();
+        public static List<CityData.Place> Citys
         {
             get { return CitySource; }
         }
 
-        public static IEnumerable<CityData.City> GetMatching(string query)
+        public static IEnumerable<CityData.Place> GetMatching(string query)
         {
             return Citys
                 .Where(c => c.name.IndexOf(query, StringComparison.CurrentCultureIgnoreCase) > -1)
                 .OrderByDescending(c => c.name.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
-                .ThenByDescending(c => c.country.StartsWith(query, StringComparison.CurrentCultureIgnoreCase));
+                .ThenByDescending(c => c.admin3.content.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
+                .ThenByDescending(c => c.admin2.content.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
+                .ThenByDescending(c => c.admin1.content.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
+                .ThenByDescending(c => c.country.content.StartsWith(query, StringComparison.CurrentCultureIgnoreCase));
         }
     }
 }
