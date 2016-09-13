3ea4a59624e791fccd0d61f6f1d556775971e18a
tracyda <tracyliubai@gmail.com>
2016-09-13 11:02:16 +0800

    Optimized code

---
+++ Weather/Weather/GaussianBlur.cs
@@ -0,0 +1,89 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Graphics.Canvas.Effects;
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Hosting;
+
+namespace Weather
+{
+    public sealed partial class MainPage : Page
+    {
+        GaussianBlurEffect blurEffect = new GaussianBlurEffect()
+        {
+            Name = "Blur",
+            BorderMode = EffectBorderMode.Hard, // NB: default mode here isn't supported yet.
+            BlurAmount = 10.0f,
+            Source = new CompositionEffectSourceParameter("source")
+        };
+
+        #region gaussian blur
+
+        SpriteVisual effectVisual;
+        SpriteVisual effectVisual2;
+
+        private void SizeVisual()
+        {
+            if (this.effectVisual != null)
+            {
+                this.effectVisual.Size = new System.Numerics.Vector2(
+                    (float)this.rightBottomBlur.ActualWidth, (float)this.rightBottomBlur.ActualHeight);
+            }
+            if (this.effectVisual2 != null)
+            {
+                this.effectVisual2.Size = new System.Numerics.Vector2(
+                    (float)this.leftBottomBlur.ActualWidth, (float)this.leftBottomBlur.ActualHeight);
+            }
+        }
+        /*
+         * Gaussian blur effect
+         */
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+
+            //Windows.UI.Composition.Visual
+            var gridVisual = ElementCompositionPreview.GetElementVisual(this.rightBottomBlur);
+            var compositor = gridVisual.Compositor;
+            effectVisual = compositor.CreateSpriteVisual();
+
+            //Windows.UI.Composition.Visual
+            var gridVisual2 = ElementCompositionPreview.GetElementVisual(this.leftBottomBlur);
+            var compositor2 = gridVisual2.Compositor;
+            effectVisual2 = compositor.CreateSpriteVisual();
+
+
+            this.SizeVisual();
+
+
+            var effectFactory = compositor.CreateEffectFactory(
+                blurEffect,
+                new string[] { "Blur.BlurAmount" }
+                );
+            var effectBrush = effectFactory.CreateBrush();
+            effectBrush.SetSourceParameter("source", compositor.CreateBackdropBrush());
+            effectVisual.Brush = effectBrush;
+
+
+            var effectFactory2 = compositor2.CreateEffectFactory(
+                blurEffect,
+                new string[] { "Blur.BlurAmount" }
+                );
+            var effectBrush2 = effectFactory2.CreateBrush();
+            effectBrush2.SetSourceParameter("source", compositor2.CreateBackdropBrush());
+            effectVisual2.Brush = effectBrush2;
+
+            ElementCompositionPreview.SetElementChildVisual(this.rightBottomBlur, this.effectVisual);
+            ElementCompositionPreview.SetElementChildVisual(this.leftBottomBlur, this.effectVisual2);
+
+
+        }
+        /*
+         * leftBottomBlur and rightBottomBlur use same size changed event
+         */
+        private void OnGridSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.SizeVisual();
+        }
+
+        #endregion
+    }
+}
---
+++ Weather/Weather/SuggestBox.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace Weather
+{
+    public sealed partial class MainPage:Page
+    {
+        #region Suggest Box
+        private void SelectCity(CityData.Place city)
+        {
+            if (city != null)
+            {
+                CityName.Text = city.name;
+                CountryCode.Text = city.country.content;
+                City1 = city.woeid;
+                TheInitialization("");
+            }
+        }
+        /*
+         * Do not use global variable anymore [CitysOfCountry]
+         * It will delay and wait the other thread
+         * Pass variable and return sorted list
+         * Display sender source
+         */
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput && !string.IsNullOrWhiteSpace(sender.Text))
+            {
+                var useless = CityData.getCityData(sender.Text).Result;
+                // RootObject mostly and then RootObject2
+                var dataContract = CityData.parseJson<CityData.RootObject>(useless);
+                if (dataContract.query.count != 0)
+                {
+                    if (dataContract.query.count != 1)
+                    {
+                        var matchingContacts = CityDataSource.GetMatching(dataContract.query.results.place.OrderBy(c => c.name).ToList(), sender.Text);
+                        //to list override
+                        sender.ItemsSource = matchingContacts.ToList();
+                    }
+                    else
+                    {
+                        var dataContract2 = CityData.parseJson<CityData.RootObject2>(useless);
+                        if (dataContract.query.count != 0)
+                        {
+                            System.Collections.Generic.List<CityData.Place> c = new System.Collections.Generic.List<CityData.Place>();
+                            c.Add(dataContract2.query.results.place);
+                            sender.ItemsSource = c;
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                SelectCity(args.ChosenSuggestion as CityData.Place);
+            }
+            else
+            {
+                NoResults.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+        }
+        /*
+         * remove this function on xaml
+         */
+        private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            var c = args.SelectedItem as CityData.Place;
+
+            sender.Text = string.Format("{0} {1}", c.name, c.country.content);
+        }
+        #endregion
+    }
+}
